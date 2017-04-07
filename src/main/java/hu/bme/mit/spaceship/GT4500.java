7ae6fa8fa563de3cdecd8c5c73d58dc1835a890c
thereallacas <lacas.shark@gmail.com>
2017-04-06 23:39:31 -0700

    removed unused variable firingsuccess

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -16,7 +16,7 @@ public class GT4500 implements SpaceShip {
   }
 
   public boolean fireLasers(FiringMode firingMode) {
-    // TODO not implemented yet
+ // TODO not implemented yet
     return false;
   }
 
